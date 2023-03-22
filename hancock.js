a01cb72737ad65326c70223e956a5030a10bc519
Eric Burrell <ericburrell231@gmail.com>
2023-03-22 15:52:56 -0500

    Code refactored, dependencies imported from cdn, Force traces overlayed onto same graph

--- hancock.js
+++ hancock.js
@@ -1,26 +1,15 @@
-import "./node_modules/lodash/lodash.js";
-import "./node_modules/plotly.js-dist/plotly.js";
-// const fs = require("fs");
-// let stream = fs.createWriteStream("hancock.txt");
+import "https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js";
+import "https://cdn.plot.ly/plotly-2.20.0.min.js";
 
 const calculateFall = (event) => {
-  let mass = document.getElementById("weight").value; // lbs
-  let drag_coeff = document.getElementById("C_drag").value;
-  let cross_sec_area = document.getElementById("CSArea").value;
-  const tower_height = document.getElementById("tower_height").value; // 1128 feet is the hight of the Hancock tower
-  const city_elevation = 597; // 597 feet is the elevation of Chicago
-  const delta_t = 0.001;
   const grav = 32.17405; // ft/s^2 at an altitude of 0 feet
+  const cityElevation = 597; // 597 feet is the elevation of Chicago
   const radius_earth = 20925722; // feet
-  function Grav(height) {
-    const altitude = height + city_elevation;
-    return grav * (radius_earth / (radius_earth + altitude));
-  }
-  function Air_Density(height) {
-    const altitude = height + city_elevation;
-    if (altitude < 5000) return 0.0765 - (2.12 / 1000000) * altitude;
-    return 0.0753 - (1.88 / 1000000) * altitude;
-  }
+  const delta_t = 0.001; // time step over which to iterate
+  const mass = document.getElementById("weight").value / grav; // lbs
+  const coeff_drag = document.getElementById("coeff_drag").value;
+  const area_cs = document.getElementById("area_cs").value; // cross sectional area of falling object ft^2
+  const height_tower = document.getElementById("height_tower").value; // 1128 feet is the height of the Hancock tower
 
   let human = {
     position: 0, // vertical feet from the top of the tower
@@ -29,25 +18,9 @@ const calculateFall = (event) => {
     velocity_last: 0,
     acceleration: 0,
     get height() {
-      return tower_height - this.position;
+      return height_tower - this.position;
     },
   };
-
-  function Net_Force(object) {
-    const F_grav = mass * Grav(object.height);
-    const F_drag =
-      0.5 *
-      drag_coeff *
-      cross_sec_area *
-      Air_Density(object.height) *
-      object.velocity *
-      object.velocity;
-    const F_net = F_grav - F_drag;
-    traceData.force_gravity.push(F_grav);
-    traceData.force_drag.push(F_drag);
-    traceData.force_net.push(F_net);
-    return F_net;
-  }
 
   let traceData = {
     height: [],
@@ -59,7 +32,34 @@ const calculateFall = (event) => {
     time: [],
   };
 
-  let fall_time = 0;
+  let fallTime = 0;
+
+  function Grav(height) {
+    const altitude = height + cityElevation;
+    return grav * (radius_earth / (radius_earth + altitude));
+  }
+
+  function Air_Density(height) {
+    const altitude = height + cityElevation;
+    if (altitude < 5000) return (0.0765 - (2.12 / 1000000) * altitude) / grav;
+    return (0.0753 - (1.88 / 1000000) * altitude) / grav;
+  }
+
+  function Net_Force(object) {
+    const F_grav = mass * Grav(object.height);
+    const F_drag =
+      0.5 *
+      coeff_drag *
+      area_cs *
+      Air_Density(object.height) *
+      object.velocity *
+      object.velocity;
+    const F_net = F_grav - F_drag;
+    traceData.force_gravity.push(F_grav);
+    traceData.force_drag.push(F_drag);
+    traceData.force_net.push(F_net);
+    return F_net;
+  }
 
   while (human.height > 0) {
     human.position =
@@ -68,19 +68,17 @@ const calculateFall = (event) => {
       human.position_last;
     human.velocity = human.acceleration * delta_t + human.velocity_last;
     human.acceleration = Net_Force(human) / mass;
-    // stream.write(
-    //   "\n" +
-    //     JSON.stringify(_.pick(human, ["position", "velocity", "acceleration"]))
-    // );
+
     traceData.height.push(human.height);
     traceData.velocity.push(human.velocity);
     traceData.acceleration.push(human.acceleration);
+
     human.position_last = human.position;
     human.velocity_last = human.velocity;
-    fall_time += delta_t;
+    fallTime += delta_t;
   }
 
-  traceData.time = _.range(0, fall_time, delta_t);
+  traceData.time = _.range(0, fallTime, delta_t);
 
   var heightData = {
     x: traceData.time,
@@ -113,6 +111,7 @@ const calculateFall = (event) => {
     x: traceData.time,
     y: traceData.force_gravity,
     type: "scatter",
+    name: "Gravity",
     line: {
       color: "purple",
     },
@@ -122,6 +121,7 @@ const calculateFall = (event) => {
     x: traceData.time,
     y: traceData.force_drag,
     type: "scatter",
+    name: "Drag",
     line: {
       color: "cyan",
     },
@@ -130,13 +130,14 @@ const calculateFall = (event) => {
     x: traceData.time,
     y: traceData.force_net,
     type: "scatter",
+    name: "Net Force",
     line: {
       color: "green",
     },
   };
 
   var heightLayout = {
-    xaxis: { range: [0, fall_time], title: "Time (s)" },
+    xaxis: { range: [0, fallTime], title: "Time (s)" },
     yaxis: {
       title: "Height (ft)",
     },
@@ -144,7 +145,7 @@ const calculateFall = (event) => {
   };
 
   var velocityLayout = {
-    xaxis: { range: [0, fall_time], title: "Time (s)" },
+    xaxis: { range: [0, fallTime], title: "Time (s)" },
     yaxis: {
       title: "Velocity (ft/s)",
     },
@@ -152,7 +153,7 @@ const calculateFall = (event) => {
   };
 
   var accelerationLayout = {
-    xaxis: { range: [0, fall_time], title: "Time (s)" },
+    xaxis: { range: [0, fallTime], title: "Time (s)" },
     yaxis: {
       min: 0,
       range: [
@@ -163,39 +164,38 @@ const calculateFall = (event) => {
     },
     title: "Faller Acceleration vs. Time",
   };
-  function ForceLayout(title) {
-    var forceLayout = {
-      xaxis: { range: [0, fall_time], title: "Time (s)" },
-      yaxis: {
-        title: "Force (lb)",
-        range: [0, _.max(traceData.force_gravity) * 1.1],
-      },
-      title: title,
-    };
-    return forceLayout;
-  }
+  var forceLayout = {
+    xaxis: { range: [0, fallTime], title: "Time (s)" },
+    yaxis: {
+      title: "Force (lb)",
+    },
+    title: "Forces at Work",
+    legend: {
+      y: 0.5,
+      traceorder: "reversed",
+      font: { size: 16 },
+      yref: "paper",
+    },
+  };
 
   // Create Plots
   Plotly.newPlot("height", [heightData], heightLayout);
   Plotly.newPlot("velocity", [velocityData], velocityLayout);
   Plotly.newPlot("acceleration", [accelerationData], accelerationLayout);
   Plotly.newPlot(
-    "force_grav",
-    [force_gravityData],
-    ForceLayout("Gravitational Force")
+    "forces",
+    [force_netData, force_dragData, force_gravityData],
+    forceLayout
   );
-  Plotly.newPlot("force_drag", [force_dragData], ForceLayout("Drag"));
-  Plotly.newPlot("force_net", [force_netData], ForceLayout("Net Force"));
 
-  console.log("Fall takes ", fall_time, "seconds");
-  console.log("Drag coefficient: ", drag_coeff);
-  let finalVelocity =
-    traceData.velocity[traceData.velocity.length - 1].toFixed(1);
-  document.getElementById("finalVelocity").innerHTML = finalVelocity.toString();
-  document.getElementById("fallTime").innerHTML = fall_time
+  let velocity_final = traceData.velocity[traceData.velocity.length - 1];
+  document.getElementById("velocity_final").innerHTML = velocity_final
+    .toFixed(1)
+    .toString();
+  document.getElementById("fallTime").innerHTML = fallTime
     .toFixed(3)
     .toString();
-  console.log("Final velocity: ", finalVelocity);
+
   if (event) {
     event.preventDefault();
   }
